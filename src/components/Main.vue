4a2da86323b48cac2a243e8e61e35f6b22f54339
sik2 <devpang20@gmail.com>
2021-09-02 11:19:23 +0900

    feat: add todo items

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,22 +2,42 @@
   <section class="main">
     <input id="toggle-all" class="toggle-all" type="checkbox"/>
     <label html-for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        <li class="completed">
-          <div class="view">
-            <input class="toggle" type="checkbox"  checked />
-            <label>Taste JavaScript</label>
-            <button class="destroy" />
-          </div>
-          <input class="edit" value="Create a TodoMVC template" />
-        </li>
+    <ul class="todo-list">
+      <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
     </ul>
   </section>
 </template>
 
 <script>
-export default {
+import Todo from './Todo.vue'
 
+let nextTodoId = 1;
+
+export default {
+  components: { Todo },
+  data () {
+    return {
+      todos: [
+        {
+          id: nextTodoId++,
+          text: "item1",
+          done: true
+        },
+        {
+          id: nextTodoId++,
+          text: "item2",
+          done: false
+        },
+        {
+          id: nextTodoId++,
+          text: "item3",
+          done: true
+        }
+      ]
+    }
+  },
+  methods: {
+  }
 }
 </script>
 
---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <li :class="{ completed: todo.done}">
+    <div class="view">
+      <input class="toggle" type="checkbox" :checked="todo.done"/>
+      <label>{{ todo.text }}</label>
+      <button class="destroy" />
+    </div>
+    <input class="edit" value="Create a TodoMVC template" />
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
